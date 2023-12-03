53c69d3d0213b9a2427e5aa9abaf770e4cd37969
Janell Smith <119452949+jellobear27@users.noreply.github.com>
2023-12-02 23:29:58 -0500

    Added pages

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,26 +1,37 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from 'react';
+import './App.css';
+import LoginPage from './pages/LoginPage';
+import { UserContextProvider } from './pages/UserContext';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Header from './pages/Header';
+import Layout from './pages/Layout';
+import CreatePost from './pages/CreatePost';
+import RegisterPage from './pages/RegisterPage';
+import EditPost from './pages/EditPost';
+import PostPage from './pages/PostPage';
+
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
 
   return (
-    <>
-      <main>
-        <header>
-          <a href="" className='logo'>CodeChronicles</a>
-          <nav>
-            <a href="Login"></a>
-            <a href="Register"></a>
-          </nav>
-        </header>
-       
-      </main>
-      
-    </>
-  )
+    <UserContextProvider>
+      <Router>
+        <Routes>
+          <Route path='/' element={<LoginPage />} />
+          <Route path="/login" element={<LoginPage />} />
+          <Route path="/layout" element={<Layout />} />
+          <Route path="/create" element={<CreatePost />} />
+          <Route path="/register" element={<RegisterPage />} />
+          <Route path="/edit/:id" element={<EditPost />} />
+          <Route path="/post/:id" element={<PostPage />} />
+          
+          {/* Add more Route components here for other paths */}
+        </Routes>
+      </Router>
+    </UserContextProvider>
+  );
 }
 
-export default App
+export default App;
+
