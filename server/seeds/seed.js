8c75e8d329a9db42f5931e3eb59a4d4801ce0311
Misachka <136197530+Misachka@users.noreply.github.com>
2023-12-06 20:46:48 -0500

    updated seeds

--- server/seeds/seed.js
+++ server/seeds/seed.js
@@ -1,17 +1,26 @@
 const db = require('../config/connection');
-const { Profile } = require('../models');
-const userseeds = require('./userSeeds.json');
+const { User, Post } = require('../models'); // Import both User and Post models
+const userSeeds = require('./userSeeds.json');
+const postSeeds = require('./postSeeds.json'); // Import post seeds
 const cleanDB = require('./cleanDB');
 
 db.once('open', async () => {
   try {
     await cleanDB('User', 'Post');
-    
-    await Profile.create(userseeds);
 
-    console.log('all done!');
+    // Insert users from userSeeds.json into the User collection
+    await User.insertMany(userSeeds);
+
+    // Insert posts from postSeeds.json into the Post collection
+    await Post.insertMany(postSeeds);
+
+    console.log('All done!');
     process.exit(0);
   } catch (err) {
-    throw err;
+    console.error('Error:', err);
+    process.exit(1);
   }
 });
+
+
+
