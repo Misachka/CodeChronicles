75a7753d1bdddccde00cf760705378dc66b2abe4
Janell Smith <119452949+jellobear27@users.noreply.github.com>
2023-12-06 19:57:25 -0500

    fix bug

--- client/src/pages/RegisterPage.jsx
+++ client/src/pages/RegisterPage.jsx
@@ -8,7 +8,7 @@ export default function RegisterPage() {
   const [password, setPassword] = useState('');
   async function register(ev) {
     ev.preventDefault();
-    const response = await fetch('http://localhost:4000/register', {
+    const response = await fetch('http://localhost:3000/api/user/register', {
       method: 'POST',
       body: JSON.stringify({username,password}),
       headers: {'Content-Type':'application/json'},
