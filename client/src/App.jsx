c08861e72e3faf6c38b6cb138b5c4850287f0ff5
K1tanaK0mbat <kkeayouth@gmail.com>
2023-12-06 19:17:49 -0600

    fixed pages and apollo

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,3 +1,9 @@
+import {
+  ApolloClient,
+  InMemoryCache,
+  ApolloProvider,
+  createHttpLink,
+} from '@apollo/client';
 import { useState } from 'react';
 import './App.css';
 import LoginPage from './pages/LoginPage';
@@ -10,45 +16,55 @@ import RegisterPage from './pages/RegisterPage';
 import EditPost from './pages/EditPost';
 import PostPage from './pages/PostPage';
 
+// Create an ApolloClient instance
+const httpLink = createHttpLink({
+  uri: 'YOUR_GRAPHQL_ENDPOINT', // Replace with your GraphQL endpoint
+});
+
+const client = new ApolloClient({
+  link: httpLink,
+  cache: new InMemoryCache(),
+});
 
 function App() {
   const [count, setCount] = useState(0);
-  const [isLoggedIn, setIsLoggedIn] = useState(true)
+  const [isLoggedIn, setIsLoggedIn] = useState(true);
 
   if (isLoggedIn) {
     return (
       <UserContextProvider>
-        <Router>
-          <Routes>
-            <Route path='/' element={<Layout isLoggedIn={isLoggedIn}/>} />
-            <Route path="/register" element={<RegisterPage />} />
-            <Route path="/login" element={<LoginPage />} />
-            <Route path="/Home" element={<Home />} />
-            <Route path="/create" element={<CreatePost />} />
-            <Route path="/edit/:id" element={<EditPost />} />
-            <Route path="/post/:id" element={<PostPage />} />
-          </Routes>
-        </Router>
+        <ApolloProvider client={client}>
+          <Router>
+            <Routes>
+              <Route path='/' element={<Layout isLoggedIn={isLoggedIn} />} />
+              <Route path="/register" element={<RegisterPage />} />
+              <Route path="/login" element={<LoginPage />} />
+              <Route path="/Home" element={<Home />} />
+              <Route path="/create" element={<CreatePost />} />
+              <Route path="/edit/:id" element={<EditPost />} />
+              <Route path="/post/:id" element={<PostPage />} />
+            </Routes>
+          </Router>
+        </ApolloProvider>
       </UserContextProvider>
     );
   } else {
     return (
       <UserContextProvider>
-        <Router>
-          <Routes>
-            <Route path='/' element={<LoginPage />} />
-            <Route path="/login" element={<LoginPage />} />
-            <Route path="/register" element={<RegisterPage />} />
-            {/* <Route path="/edit/:id" element={<EditPost />} />
-            <Route path="/post/:id" element={<PostPage />} /> */}
-          </Routes>
-        </Router>
+        <ApolloProvider client={client}>
+          <Router>
+            <Routes>
+              <Route path='/' element={<LoginPage />} />
+              <Route path="/login" element={<LoginPage />} />
+              <Route path="/register" element={<RegisterPage />} />
+              {/* <Route path="/edit/:id" element={<EditPost />} />
+              <Route path="/post/:id" element={<PostPage />} /> */}
+            </Routes>
+          </Router>
+        </ApolloProvider>
       </UserContextProvider>
     );
   }
-
-  
 }
 
 export default App;
-
