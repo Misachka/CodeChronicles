b551d856df5061a25bcf948e190e2b14ce78cb07
Janell Smith <119452949+jellobear27@users.noreply.github.com>
2023-12-10 21:33:03 -0500

    fixed home page bug

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,33 +1,65 @@
-import {Link} from "react-router-dom";
-import {useContext, useEffect, useState} from "react";
-import {UserContext} from "./UserContext";
-import {  useQuery } from '@apollo/client';
-import { GET_ALL_POSTS } from "../utils/queries";
+import { Link } from 'react-router-dom';
+import { useContext, useEffect } from 'react';
+import { UserContext } from './UserContext';
+import { useQuery } from '@apollo/client';
+import { GET_ALL_POSTS } from '../utils/queries';
 
-// export default function Home(props) {
-//   const {setUserInfo,userInfo} = useContext(UserContext);
-  
-  // useEffect(() => {
-  //   fetch('http://localhost:3001/profile', {
-  //     credentials: 'include',
-  //   }).then(response => {
-  //     response.json().then(userInfo => {
-  //       setUserInfo(userInfo);
-  //     });
-  //   });
-  // }, []);
+function Home(props) {
+  const { setUserInfo, userInfo } = useContext(UserContext);
 
-  // function logout() {
-  //   fetch('http://localhost:3000/logout', {
-  //     credentials: 'include',
-  //     method: 'POST',
-  //   });
-  //   setUserInfo(null);
-  // }
+  useEffect(() => {
+    fetch('http://localhost:3001/profile', {
+      credentials: 'include',
+    }).then(response => {
+      response.json().then(userInfo => {
+        setUserInfo(userInfo);
+      });
+    });
+  }, []);
 
-  // const username = userInfo?.username;
+  function logout() {
+    fetch('http://localhost:3000/logout', {
+      credentials: 'include',
+      method: 'POST',
+    });
+    setUserInfo(null);
+  }
 
-export default function postList () {
+  const username = userInfo?.username;
+
+  return (
+    <>
+      <header>
+        <Link to="/" className="logo">
+          Code Chronicles
+        </Link>
+        <nav>
+          {username && (
+            <>
+              <Link to="/create">Create new post</Link>
+              <a onClick={logout}>Logout ({username})</a>
+            </>
+          )}
+          {!username && (
+            <>
+              <Link to="/login">Login</Link>
+              <Link to="/register">Register</Link>
+            </>
+          )}
+        </nav>
+      </header>
+
+      <div>
+        <h2>All Posts</h2>
+        {/* PostList component here */}
+        <PostList />
+      </div>
+    </>
+  );
+}
+
+// Define PostList component outside of the Home component
+function PostList() {
   const { loading, data } = useQuery(GET_ALL_POSTS);
 
   if (loading) {
@@ -35,46 +67,21 @@ export default function postList () {
   }
 
   if (!data || !data.getAllPosts) {
-    return <p>No posts at this moment...</p>
+    return <p>No posts at this moment...</p>;
   }
 
-  
-  
-
   return (
-    <>
-    {/* <header>
-      <Link to="/" className="logo">Code Chronicles</Link>
-      <nav>
-        {username && (
-          <>
-            <Link to="/create">Create new post</Link>
-            <a onClick={logout}>Logout ({username})</a>
-          </>
-        )}
-        {true &&  (
-          <>
-            <Link to="/login">Login</Link>
-            <Link to="/register">Register</Link>
-          </>
-        )}
-      </nav>
-    </header> */}
-
-<div>
-<h2>All Posts</h2>
-<ul>
-  {data.getAllPosts.map((post) => (
-    <li key={post._id}>
-      <h3>{post.title}</h3>
-      <p>{post.content}</p>
-      <p>Author: {post.username.username}</p>
-    </li>
-  ))}
-</ul>
-</div>
-</>
-
-    
+    <ul>
+      {data.getAllPosts.map((post) => (
+        <li key={post._id}>
+          <h3>{post.title}</h3>
+          <p>{post.content}</p>
+          <p>Author: {post.username.username}</p>
+        </li>
+      ))}
+    </ul>
   );
-};
+}
+
+export default Home; // Export the Home component as the default export
+
