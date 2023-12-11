394d79049f1497b35d44ba462db7483086626663
Yemny <y.feliz1296@gmail.com>
2023-12-10 19:05:34 -0500

    logic to get all posts to show up, few changes in the resolvers

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,9 +1,11 @@
 import {Link} from "react-router-dom";
 import {useContext, useEffect, useState} from "react";
 import {UserContext} from "./UserContext";
+import {  useQuery } from '@apollo/client';
+import { GET_ALL_POSTS } from "../utils/queries";
 
-export default function Home(props) {
-  const {setUserInfo,userInfo} = useContext(UserContext);
+// export default function Home(props) {
+//   const {setUserInfo,userInfo} = useContext(UserContext);
   
   // useEffect(() => {
   //   fetch('http://localhost:3001/profile', {
@@ -15,18 +17,33 @@ export default function Home(props) {
   //   });
   // }, []);
 
-  function logout() {
-    fetch('http://localhost:3000/logout', {
-      credentials: 'include',
-      method: 'POST',
-    });
-    setUserInfo(null);
+  // function logout() {
+  //   fetch('http://localhost:3000/logout', {
+  //     credentials: 'include',
+  //     method: 'POST',
+  //   });
+  //   setUserInfo(null);
+  // }
+
+  // const username = userInfo?.username;
+
+export default function postList () {
+  const { loading, data } = useQuery(GET_ALL_POSTS);
+
+  if (loading) {
+    return <p>Loading Posts...</p>;
   }
 
-  const username = userInfo?.username;
+  if (!data || !data.getAllPosts) {
+    return <p>No posts at this moment...</p>
+  }
+
+  
+  
 
   return (
-    <header>
+    <>
+    {/* <header>
       <Link to="/" className="logo">Code Chronicles</Link>
       <nav>
         {username && (
@@ -42,6 +59,22 @@ export default function Home(props) {
           </>
         )}
       </nav>
-    </header>
+    </header> */}
+
+<div>
+<h2>All Posts</h2>
+<ul>
+  {data.getAllPosts.map((post) => (
+    <li key={post._id}>
+      <h3>{post.title}</h3>
+      <p>{post.content}</p>
+      <p>Author: {post.username.username}</p>
+    </li>
+  ))}
+</ul>
+</div>
+</>
+
+    
   );
-}
+};
